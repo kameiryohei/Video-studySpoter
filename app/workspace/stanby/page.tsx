06b1b48a3c3b2b1f6371bb59a42920c80cc88596
tasukuwato <221205191@ccmailg.meijo-u.ac.jp>
2024-03-28 07:18:27 +0900

    add: stanby menu

--- app/workspace/stanby/page.tsx
+++ app/workspace/stanby/page.tsx
@@ -1,20 +1,76 @@
-import { Slider } from "@/components/ui/slider";
-import Link from "next/link";
+"use client";
 
-const page = () => {
+import { Slider } from "@/components/ui/slider";
+import OT from "@opentok/client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const SESSION_ID = process.env.NEXT_PUBLIC_SESSION_ID;
+const TOKEN = process.env.NEXT_PUBLIC_TOKEN;
+
+function handleError(error: any) {
+  if (!error) return;
+  alert(error.message);
+}
+
+const Page = () => {
+  const [publisher, setPublisher] = useState<any>(null);
+
+  useEffect(() => {
+    if (!API_KEY || !SESSION_ID || !TOKEN) {
+      alert("API_KEY, SESSION_ID, and TOKEN is required");
+      return;
+    }
+    const session = OT.initSession(API_KEY, SESSION_ID);
+
+    const initPublisher = OT.initPublisher(
+      "myCamera",
+      {
+        insertMode: "append",
+        width: "100%",
+        height: "100%",
+      },
+      handleError
+    );
+    setPublisher(initPublisher);
+
+    session.connect(TOKEN, function (error) {
+      if (error) {
+        handleError(error);
+        return;
+      }
+      session.publish(initPublisher, handleError);
+    });
+
+    return () => {
+      if (session) {
+        session.disconnect();
+      }
+      if (initPublisher) {
+        initPublisher.destroy();
+      }
+    };
+  }, []);
+
   return (
     <div className="px-24 mt-10">
+      <Link href="/">
+        <p className="border-b-blue-500 text-3xl font-bold top-0 outline-2 cursor-pointer">Home</p>
+      </Link>
       <div className="flex justify-center">
-        <div className="bg-gray-600 w-[1244px] h-[732px] rounded-2xl">aa</div>
+        <div id="myCamera" className="bg-gray-600 w-[1244px] h-[732px] rounded-2xl"></div>
       </div>
-      <div className="flex mt-5 justify-center">
-        <Link href="/workspace/stanby/video">
-          <div className="bg-gray-500 w-[190px] h-[190px] rounded-full" />
-        </Link>
-        <Slider defaultValue={[33]} max={100} step={1} className="w-[466px]" />
+      <div className="flex justify-center mt-5">
+        <div className="flex justify-end items-center w-[1244px]">
+          <Link href="/workspace/stanby/video" className="bg-gray-500 w-[190px] h-[190px] rounded-full flex items-center justify-center text-white mr-4">
+            JOIN
+          </Link>
+          <Slider defaultValue={[33]} max={100} step={1} className="w-[466px]" />
+        </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
