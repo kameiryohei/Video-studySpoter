c78a9789d22523300e2bea0bc536818249668468
ito <88521121+HarumaIto@users.noreply.github.com>
2024-03-28 12:46:25 +0900

    add ランキングの一覧を取得する

--- app/rank/page.tsx
+++ app/rank/page.tsx
@@ -1,6 +1,38 @@
-import React from "react";
+'use client'
+
+import React, { useEffect } from "react";
+
+import { initializeApp } from "firebase/app";
+import firebaseConfig from '../firebase_config';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { rankingData } from "./ranking_data";
 
 const page = () => {
+  const [ranking, setRanking] = React.useState<rankingData[]>([]);
+
+  // 読み込み時にランキングを作成
+  useEffect(() => {
+    const fetchData = async () => {
+      initializeApp(firebaseConfig);
+      const db = getFirestore();
+      const querySnapshot = await getDocs(collection(db, "users"));
+
+      var rankings: rankingData[] = [];
+      querySnapshot.forEach((doc) => {
+        rankings.push({
+            uid: doc.id,
+            email: doc.data().email,
+            points: doc.data().points,
+          }
+        );
+      });
+      rankings.sort((a, b) => a.points + b.points);
+      console.log(rankings);
+      setRanking(rankings);
+    };
+    fetchData();
+  }, []);
+
   return <div>page</div>;
 };
 
