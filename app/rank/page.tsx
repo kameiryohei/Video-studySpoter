a743703b0f17e476025e1bcc27eb3c80a19e9ec6
ito <88521121+HarumaIto@users.noreply.github.com>
2024-03-28 12:52:15 +0900

    add ランキングを表示するUIの追加

--- app/rank/page.tsx
+++ app/rank/page.tsx
@@ -20,10 +20,10 @@ const page = () => {
       var rankings: rankingData[] = [];
       querySnapshot.forEach((doc) => {
         rankings.push({
-            uid: doc.id,
-            email: doc.data().email,
-            points: doc.data().points,
-          }
+          uid: doc.id,
+          email: doc.data().email,
+          points: doc.data().points,
+        }
         );
       });
       rankings.sort((a, b) => a.points + b.points);
@@ -33,7 +33,29 @@ const page = () => {
     fetchData();
   }, []);
 
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>ランキング</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>順位</th>
+            <th>メールアドレス</th>
+            <th>ポイント</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ranking.map((data, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{data.email}</td>
+              <td>{data.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default page;
