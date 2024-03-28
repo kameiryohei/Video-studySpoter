8145c5b93a1bd06aac27bf56f2a312a500795710
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-03-28 11:41:19 +0900

    関数のエラーを解消

--- app/workspace/stanby/page.tsx
+++ app/workspace/stanby/page.tsx
@@ -5,9 +5,9 @@ import OT from "@opentok/client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-const SESSION_ID = process.env.NEXT_PUBLIC_SESSION_ID;
-const TOKEN = process.env.NEXT_PUBLIC_TOKEN;
+const API_KEY = process.env.NEXT_PUBLIC_OPENTOK_API_KEY;
+const SESSION_ID = process.env.NEXT_PUBLIC_OPENTOK_SESSION_ID;
+const TOKEN = process.env.NEXT_PUBLIC_OPENTOK_TOKEN;
 
 function handleError(error: any) {
   if (!error) return;
@@ -54,24 +54,38 @@ const Page = () => {
     };
   }, []);
 
-  const handleBlurChange = (value: number) => {
+  const handleBlurChange = (value: any) => {
     setBlurAmount(value);
   };
 
   return (
     <div className="px-24 mt-10">
       <Link href="/">
-        <p className="border-b-blue-500 text-3xl font-bold top-0 outline-2 cursor-pointer">Home</p>
+        <p className="border-b-blue-500 text-3xl font-bold top-0 outline-2 cursor-pointer">
+          Home
+        </p>
       </Link>
       <div className="flex justify-center">
-        <div id="myCamera" className="bg-gray-600 w-[1244px] h-[732px] rounded-2xl"></div>
+        <div
+          id="myCamera"
+          className="bg-gray-600 w-[1244px] h-[732px] rounded-2xl"
+        ></div>
       </div>
       <div className="flex justify-center mt-5">
         <div className="flex justify-end items-center w-[1244px]">
-          <Link href="/workspace/stanby/video" className="bg-gray-500 w-[190px] h-[190px] rounded-full flex items-center justify-center text-white mr-4">
+          <Link
+            href="/workspace/stanby/video"
+            className="bg-gray-500 w-[190px] h-[190px] rounded-full flex items-center justify-center text-white mr-4"
+          >
             JOIN
           </Link>
-          <Slider defaultValue={[0]} max={1000} step={1} className="w-[466px]" onChange={(value) => handleBlurChange(value)} />
+          <Slider
+            defaultValue={[0]}
+            max={1000}
+            step={1}
+            className="w-[466px]"
+            onChange={(value) => handleBlurChange(value)}
+          />
         </div>
       </div>
     </div>
